7d2dc74261dad21cc5c3ba415d4f69370317a0bc
haaddii <hardywong_hw@icloud.com>
2022-04-27 19:58:30 +0800

    update

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,10 +1,29 @@
 -- base
-vim.o.background = "dark"
 vim.o.termguicolors = true
 vim.opt.termguicolors = true
 
 -- no mode in status line
 vim.o.showmode = false
+
+-- get macOS Darkmode status and choose background mode
+local auto_status_file = io.popen("defaults read -g AppleInterfaceStyleSwitchesAutomatically")
+local dark_status_file = io.popen("defaults read -g AppleInterfaceStyle")
+local auto_status = auto_status_file:read("*all")
+local dark_status = dark_status_file:read("*all")
+
+if auto_status == "1\n" then
+  if dark_status == "Dark\n" then
+    vim.o.background = "dark"
+  else
+    vim.o.background = "light"
+  end
+else
+  if dark_status == "Dark\n" then
+    vim.o.background = "dark"
+  else
+    vim.o.background = "light"
+  end
+end
 
 local colorscheme = "tokyonight"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
